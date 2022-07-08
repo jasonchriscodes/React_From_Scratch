df0ced9fa54f39242ab367385b7ddd5edb547cc9
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-08 22:14:39 +1200

    Learning let and const

---
+++ JavaScript Refresher/1_Let_And_Const.js
@@ -0,0 +1,17 @@
+// run in jsbin.com
+
+// var myName = "Max";
+// console.log(myName);
+// myName = "Manu";
+// console.log(myName);
+
+// alternative
+// let myName = "Max";
+// console.log(myName);
+// myName = "Manu";
+// console.log(myName);
+
+const myName = "Max";
+console.log(myName);
+myName = "Manu";
+console.log(myName); // will throw arrow, reasigning isnt allow in const
